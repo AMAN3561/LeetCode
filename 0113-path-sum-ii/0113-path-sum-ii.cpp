ad1a82692a2f5078da5a1865d10da068957f8c99
Aman Khan <amanx11n@gmail.com>
2026-07-15 23:04:32 +0530

    Time: 0 ms (100%), Space: 20.8 MB (99.55%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void solve(TreeNode* root, int targetSum, int sum, vector<int>& temp, vector<vector<int>>& ans){
+        if(root == NULL) return;
+
+        // curr node pr :->
+        sum += root->val;
+        temp.push_back(root->val);
+
+        if(root->left == NULL && root->right == NULL){
+            if(sum == targetSum){
+                ans.push_back(temp);
+                temp.pop_back(); // necessary if we are passing the temp by reference.
+                return;
+            }
+        }
+        // recursion :
+        solve(root->left, targetSum, sum, temp, ans);
+        solve(root->right, targetSum, sum, temp, ans);
+
+        // if we are passing the temp by reference then it is necessary :
+        temp.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        int sum = 0;
+        vector<int> temp;
+        vector<vector<int>> ans;
+        solve(root, targetSum, sum, temp, ans);
+        return ans;
+    }
+};
