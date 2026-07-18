01c0d4647e06a80ef55989c97b299b8bf7f27df2
Aman Khan <amanx11n@gmail.com>
2026-07-18 19:32:33 +0530

    Time: 0 ms (100%), Space: 20.6 MB (52.58%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -12,23 +12,60 @@
 class Solution {
 public:
     long long total = 0;
-    void path(TreeNode* root, int targetSum, long long sum){
-        if(root == NULL){
-            return ;
-        }
+    void path(TreeNode* root, int targetSum, long long sum, unordered_map<long long, int>& mp){
+        if (root == nullptr)
+            return;
         sum += root->val;
-        if(sum == targetSum){
-            total++;
-        }
-        path(root->left, targetSum, sum);
-        path(root->right, targetSum, sum);
+        if (mp.count(sum - targetSum))
+            total += mp[sum - targetSum];
+        mp[sum]++;
+        path(root->left, targetSum, sum, mp);
+        path(root->right, targetSum, sum, mp);
+        mp[sum]--;
     }
     int pathSum(TreeNode* root, int targetSum) {
-        long long sum = 0;
-        if(root == NULL) return 0;
-        path(root, targetSum, sum);
-        pathSum(root->left, targetSum);
-        pathSum(root->right, targetSum);
+        if (root == nullptr)
+            return 0;
+        unordered_map<long long, int> mp;
+        mp[0] = 1;
+        path(root, targetSum, 0, mp);
         return total;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     long long total = 0;
+//     void path(TreeNode* root, int targetSum, long long sum){
+//         if(root == NULL){
+//             return ;
+//         }
+//         sum += root->val;
+//         if(sum == targetSum){
+//             total++;
+//         }
+//         path(root->left, targetSum, sum);
+//         path(root->right, targetSum, sum);
+//     }
+//     int pathSum(TreeNode* root, int targetSum) {
+//         long long sum = 0;
+//         if(root == NULL) return 0;
+//         path(root, targetSum, sum);
+//         pathSum(root->left, targetSum);
+//         pathSum(root->right, targetSum);
+//         return total;
+//     }
+// };
