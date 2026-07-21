c33be2b587fa3b4b9ec4b079d53660d0b226e1aa
Aman Khan <amanx11n@gmail.com>
2026-07-21 15:37:21 +0530

    Time: 0 ms (100%), Space: 11.6 MB (5.46%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void makeInorder(TreeNode* root, vector<int>& inorder){
+        if(root == NULL){
+            return;
+        }
+        // LNR :
+        // L.
+        makeInorder(root->left, inorder);
+        // N.
+        inorder.push_back(root->val);
+        // R.
+        makeInorder(root->right, inorder);
+    }
+    void updateTree(TreeNode* root, vector<int>& inorder, int& index){
+        if(root == NULL){
+            return;
+        }
+        // LNR :
+        updateTree(root->left, inorder, index);
+        root->val = inorder[index];
+        index++;
+        updateTree(root->right, inorder, index);
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+        if(root == NULL){
+            return NULL;
+        }
+        if(root->left == NULL && root->right == NULL){
+            return root;
+        }
+        // step 1 : store inorder.
+        vector<int> inorder;
+        makeInorder(root, inorder);
+        int n = inorder.size();
+        // step 2 : update inorder as per question.
+        for(int i = n-1; i>=0; i--){
+            int currValue = inorder[i];
+            int nextVal = 0;
+            if(i + 1 < n){
+                nextVal = inorder[i + 1];
+            }
+            int sum = currValue + nextVal;
+            // replacement :
+            inorder[i] = sum;
+        }
+        // step 3 : update tree using inorder
+        int index = 0;
+        updateTree(root, inorder, index);
+        return root;
+    }
+};
