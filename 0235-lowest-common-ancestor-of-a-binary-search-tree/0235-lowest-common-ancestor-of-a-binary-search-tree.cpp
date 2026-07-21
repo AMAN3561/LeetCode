5b8b4cbcf71ec1b37425925ade5fd695b91434e4
Aman Khan <amanx11n@gmail.com>
2026-07-21 17:24:51 +0530

    Time: 17 ms (92.66%), Space: 23.6 MB (36.21%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,32 +9,60 @@
  */
 
 class Solution {
-public:
+public: // optimised approach :
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(root == NULL){
             return NULL;
         }
-        if(root->val == p->val){
-            return p;
+        // Case 1 : p & q are both are on the leftsubTree
+        // rightSubtree ko neglect kro
+        if(p->val < root->val && q->val < root->val){
+            TreeNode* Leftans = lowestCommonAncestor(root->left, p, q);
+            if(Leftans != NULL){
+                return Leftans;
+            }
         }
-        if(root->val == q->val){
-            return q;
+        // Case 2 : p & q are both are on the rightsubTree
+        // leftsubTree ko neglect kro
+        if(p->val > root->val && q->val > root->val){
+            TreeNode* Rightans = lowestCommonAncestor(root->right, p, q);
+            if(Rightans != NULL){
+                return Rightans;
+            }
         }
-        TreeNode* leftans = lowestCommonAncestor(root->left, p, q);
-        TreeNode* rightans = lowestCommonAncestor(root->right, p, q);
-        
-        if(leftans == NULL && rightans == NULL){
-            return NULL;
-        }
-        else if(leftans == NULL && rightans != NULL){
-            return rightans;
-        }
-        else if(leftans != NULL && rightans == NULL){
-            return leftans;
-        }
-        else{
-            // leftans != NULL && rightans != NULL
-            return root;
-        }
+        // Case 3 : p in leftsubTree & q in RightsubTree
+        // Case 4 : p in RightsubTree & q in leftsubTree
+        return root; // for both case 3,4.
     }
 };
+
+// class Solution {
+// public:
+//     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+//         if(root == NULL){
+//             return NULL;
+//         }
+//         if(root->val == p->val){
+//             return p;
+//         }
+//         if(root->val == q->val){
+//             return q;
+//         }
+//         TreeNode* leftans = lowestCommonAncestor(root->left, p, q);
+//         TreeNode* rightans = lowestCommonAncestor(root->right, p, q);
+        
+//         if(leftans == NULL && rightans == NULL){
+//             return NULL;
+//         }
+//         else if(leftans == NULL && rightans != NULL){
+//             return rightans;
+//         }
+//         else if(leftans != NULL && rightans == NULL){
+//             return leftans;
+//         }
+//         else{
+//             // leftans != NULL && rightans != NULL
+//             return root;
+//         }
+//     }
+// };
