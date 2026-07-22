c0498585c7d5bed1282ec73374b014f17238c937
Aman Khan <amanx11n@gmail.com>
2026-07-22 19:35:37 +0530

    Time: 0 ms (100%), Space: 14 MB (75.33%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -2,24 +2,52 @@ class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
         int n = arr.size();
-        vector<int> dest(n);
-        int s = 0;
-        int d = 0;
-        while (s < n) {
-            if (arr[s] == 0) {
-                if (d < n)
-                    dest[d] = 0;
-                d++;
-                if (d < n)
-                    dest[d] = 0;
-            } 
-            else {
-                if (d < n)
-                    dest[d] = arr[s];
-            }
-            s++;
-            d++;
+        int zeros = 0;
+        // Count zeros
+        for (int x : arr) {
+            if (x == 0)
+                zeros++;
         }
-        arr = dest;
+        int i = n - 1;
+        int j = n + zeros - 1;
+        while (i >= 0) {
+            if (j < n)
+                arr[j] = arr[i];
+
+            if (arr[i] == 0) {
+                j--;
+                if (j < n)
+                    arr[j] = 0;
+            }
+            i--;
+            j--;
+        }
     }
 };
+
+
+// class Solution {
+// public:
+//     void duplicateZeros(vector<int>& arr) {
+//         int n = arr.size();
+//         vector<int> dest(n);
+//         int s = 0;
+//         int d = 0;
+//         while (s < n) {
+//             if (arr[s] == 0) {
+//                 if (d < n)
+//                     dest[d] = 0;
+//                 d++;
+//                 if (d < n)
+//                     dest[d] = 0;
+//             } 
+//             else {
+//                 if (d < n)
+//                     dest[d] = arr[s];
+//             }
+//             s++;
+//             d++;
+//         }
+//         arr = dest;
+//     }
+// };
