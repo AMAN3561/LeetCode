e51a0db53e9436215adca32bd1b23808e9bda47a
Aman Khan <amanx11n@gmail.com>
2026-07-23 20:05:01 +0530

    Time: 0 ms (100%), Space: 7.9 MB (68.27%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
-    bool isHappy(int n) {
-        unordered_set<int> seen;
-        while(n != 1 && !seen.count(n)){
-            seen.insert(n);
-            int sum = 0;
-            while(n > 0){
-                int digit = n % 10;
-                sum += digit*digit;
-                n /= 10;
-            }
-            n = sum;
+    int getNext(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
         }
-        return n == 1;
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do {
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
+        } while (slow != fast);
+        return slow == 1;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isHappy(int n) {
+//         unordered_set<int> seen;
+//         while(n != 1 && !seen.count(n)){
+//             seen.insert(n);
+//             int sum = 0;
+//             while(n > 0){
+//                 int digit = n % 10;
+//                 sum += digit*digit;
+//                 n /= 10;
+//             }
+//             n = sum;
+//         }
+//         return n == 1;
+//     }
+// };
