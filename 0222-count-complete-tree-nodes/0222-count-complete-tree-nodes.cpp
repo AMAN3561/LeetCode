c317e446e7f9e843fc24fa2807515a8662c83348
Aman Khan <amanx11n@gmail.com>
2026-07-24 16:55:01 +0530

    Time: 0 ms (100%), Space: 31.2 MB (95.81%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -9,22 +9,59 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
 public:
-    int count = 0;
-    void inorder(TreeNode* root){
-        if(root == nullptr) return;
-
-        // LNR :
-        // L :
-        inorder(root->left);
-        // N :
-        count++;
-        // R :
-        inorder(root->right);
+    int leftheight(TreeNode* root){
+        int h = 0;
+        while(root){
+            h++;
+            root = root->left;
+        }
+        return h;
     }
-    int countNodes(TreeNode* root) {
-        inorder(root);
-        return count;
+    int rightheight(TreeNode* root){
+        int h = 0;
+        while(root){
+            h++;
+            root = root->right;
+        }
+        return h;
+    }
+    int countNodes(TreeNode* root){
+        if(root == nullptr) return 0;
+
+        int lh = leftheight(root);
+        int rh = rightheight(root);
+
+        if(lh == rh){
+            // FCBT :
+            return (1 << lh) - 1;
+        }
+        // lh != rh :
+        int countleft = countNodes(root->left);
+        int countright = countNodes(root->right);
+        return countleft + countright + 1;
     }
 };
+
+
+// class Solution {
+// public:
+//     int count = 0;
+//     void inorder(TreeNode* root){
+//         if(root == nullptr) return;
+
+//         // LNR :
+//         // L :
+//         inorder(root->left);
+//         // N :
+//         count++;
+//         // R :
+//         inorder(root->right);
+//     }
+//     int countNodes(TreeNode* root) {
+//         inorder(root);
+//         return count;
+//     }
+// };
