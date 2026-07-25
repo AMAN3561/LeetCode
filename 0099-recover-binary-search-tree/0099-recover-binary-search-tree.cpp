bb380a0577d38e24e32faa8ba1fec5345bd85b31
Aman Khan <amanx11n@gmail.com>
2026-07-26 01:29:17 +0530

    Time: 0 ms (100%), Space: 60.7 MB (79.54%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.cpp
@@ -6,46 +6,81 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root, vector<int>& inorder){
-        if(root == nullptr) return ;
+    TreeNode* first = nullptr;
+    TreeNode* second = nullptr;
+    TreeNode* prev = nullptr;
+    void Inorder(TreeNode* root) {
+        if (root == nullptr)
+            return;
         // LNR :
         // L :
-        solve(root->left, inorder); 
-        // N : 
-        inorder.push_back(root->val);
-        // R :
-        solve(root->right, inorder);
-    }
-    void buldTree(TreeNode* root, vector<int>& inorder, int& index){
-        if(root == nullptr){
-            return ;
+        Inorder(root->left);
+        // N :
+        //.. violationg nodes detection :
+        if (prev != nullptr && root->val < prev->val) {
+            if (!first) {
+                first = prev; // first violating node
+            }
+            second = root; // second violation node
         }
-        buldTree(root->left, inorder, index);
-        root->val = inorder[index];
-        index++;
-        buldTree(root->right, inorder, index);
+        prev = root;
+        // R :
+        Inorder(root->right);
     }
     void recoverTree(TreeNode* root) {
-        vector<int> inorder;
-        int index = 0;
-        solve(root, inorder);
-        // sort(inorder.begin(), inorder.end());
-        int first = -1;
-        int second = -1;
-        for (int i = 0; i < inorder.size() - 1; i++) {
-            if (inorder[i] > inorder[i + 1]) {
-                if (first == -1) {
-                    first = i;
-                }
-            second = i + 1;
+        Inorder(root);
+
+        // at this point i'll having both violating nodes , just swap them
+        if (first && second) {
+            swap(first->val, second->val);
         }
-}
-swap(inorder[first], inorder[second]);
-        buldTree(root, inorder, index);
     }
 };
+
+// class Solution {
+// public:
+//     void solve(TreeNode* root, vector<int>& inorder) {
+//         if (root == nullptr)
+//             return;
+//         // LNR :
+//         // L :
+//         solve(root->left, inorder);
+//         // N :
+//         inorder.push_back(root->val);
+//         // R :
+//         solve(root->right, inorder);
+//     }
+//     void buldTree(TreeNode* root, vector<int>& inorder, int& index) {
+//         if (root == nullptr) {
+//             return;
+//         }
+//         buldTree(root->left, inorder, index);
+//         root->val = inorder[index];
+//         index++;
+//         buldTree(root->right, inorder, index);
+//     }
+//     void recoverTree(TreeNode* root) {
+//         vector<int> inorder;
+//         int index = 0;
+//         solve(root, inorder);
+//         // sort(inorder.begin(), inorder.end());
+//         int first = -1;
+//         int second = -1;
+//         for (int i = 0; i < inorder.size() - 1; i++) {
+//             if (inorder[i] > inorder[i + 1]) {
+//                 if (first == -1) {
+//                     first = i;
+//                 }
+//                 second = i + 1;
+//             }
+//         }
+//         swap(inorder[first], inorder[second]);
+//         buldTree(root, inorder, index);
+//     }
+// };
