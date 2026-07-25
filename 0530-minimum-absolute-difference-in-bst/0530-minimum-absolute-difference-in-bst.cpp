bf580b61284dd7e011e8d939a96ef9ef393908d5
Aman Khan <amanx11n@gmail.com>
2026-07-25 23:00:58 +0530

    Time: 0 ms (100%), Space: 25.3 MB (89.23%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.cpp
@@ -11,20 +11,43 @@
  */
 class Solution {
 public:
-    void Ino(TreeNode* root, vector<int>& arr){
+    void solve(TreeNode* root, TreeNode*& prev, int& ans){
         if(root == nullptr) return ;
 
-        Ino(root->left, arr);
-        arr.push_back(root->val);
-        Ino(root->right, arr);
+        solve(root->left, prev, ans);
+        // N :
+        if(prev){
+            ans = min(ans, root->val - prev->val);
+        }
+        prev = root;
+        solve(root->right, prev, ans);
     }
     int getMinimumDifference(TreeNode* root) {
-        int difference = INT_MAX;
-        vector<int> arr;
-        Ino(root, arr);
-        for(int i = 1; i<arr.size(); i++){
-            difference = min(difference, arr[i] - arr[i - 1]);
-        }
-        return difference;
+        TreeNode* prev = 0;
+        int ans = INT_MAX;
+        solve(root, prev, ans);
+        return ans;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     void Ino(TreeNode* root, vector<int>& arr){
+//         if(root == nullptr) return ;
+
+//         Ino(root->left, arr);
+//         arr.push_back(root->val);
+//         Ino(root->right, arr);
+//     }
+//     int getMinimumDifference(TreeNode* root) {
+//         int difference = INT_MAX;
+//         vector<int> arr;
+//         Ino(root, arr);
+//         for(int i = 1; i<arr.size(); i++){
+//             difference = min(difference, arr[i] - arr[i - 1]);
+//         }
+//         return difference;
+//     }
+// };
