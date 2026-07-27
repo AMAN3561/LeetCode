390cd77744279c93b84aa7eb6b18be09588852e1
Aman Khan <amanx11n@gmail.com>
2026-07-27 17:40:54 +0530

    Time: 0 ms (100%), Space: 23.3 MB (93.74%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -11,29 +11,53 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     bool isPalindrome(string &word){
+//         int l = 0;
+//         int r = word.length() - 1;
+//         while( l < r){
+//             if(word[l] != word[r]){
+//                 return false;
+//             }
+//             l++;
+//             r--;
+//         }
+//         return true;
+//     }
+//     string firstPalindrome(vector<string>& words) {
+//         for(string &word : words){
+//             if(isPalindrome(word)){
+//                 return word;
+//             }
+//         }
+//         return "";
+//     }
+// };
+
+
+
 class Solution {
 public:
-    bool isPalindrome(string &word){
-        int l = 0;
-        int r = word.length() - 1;
-        while( l < r){
-            if(word[l] != word[r]){
-                return false;
-            }
-            l++;
-            r--;
-        }
-        return true;
-    }
     string firstPalindrome(vector<string>& words) {
-        for(string &word : words){
-            if(isPalindrome(word)){
-                return word;
+        for (int i = 0; i < words.size(); i++) {
+            int left = 0;
+            int right = words[i].size() - 1;
+            bool palindrome = true;
+            while (left < right) {
+                if (words[i][left] != words[i][right]) {
+                    palindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+            if (palindrome) {
+                return words[i];
             }
         }
         return "";
     }
 };
-
 
 
