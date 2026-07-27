d8af9e1a3f8404e51536a92555ea22f084a99dea
Aman Khan <amanx11n@gmail.com>
2026-07-27 17:32:05 +0530

    Time: 1 ms (68.98%), Space: 23.2 MB (93.74%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -1,12 +1,39 @@
+// class Solution {
+// public:
+//     string firstPalindrome(vector<string>& words) {
+//         for(string &word : words){
+//             int n = word.size();
+//             if(equal(word.begin(), word.begin()+ n/2, word.rbegin())){
+//                 return word;
+//             }
+//         }
+//         return "";
+//     }
+// };
+
 class Solution {
 public:
+    bool isPalindrome(string &word){
+        int l = 0;
+        int r = word.length() - 1;
+        while( l < r){
+            if(word[l] != word[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
     string firstPalindrome(vector<string>& words) {
         for(string &word : words){
-            int n = word.size();
-            if(equal(word.begin(), word.begin()+ n/2, word.rbegin())){
+            if(isPalindrome(word)){
                 return word;
             }
         }
         return "";
     }
 };
+
+
+
