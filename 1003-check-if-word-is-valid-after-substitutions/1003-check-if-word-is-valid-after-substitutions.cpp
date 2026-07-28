0bbb9a5e4e6c92a8952a9cbd84c43b7dbb6c4282
Aman Khan <amanx11n@gmail.com>
2026-07-28 19:37:59 +0530

    Time: 1 ms (71.37%), Space: 12.1 MB (18.13%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,16 +1,51 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() == 0){
-            return true;
+        if(s[0] != 'a') return false;
+        stack<char> st;
+        for(char ch : s){
+            if(ch == 'a'){
+                st.push(ch);
+            }
+            else if(ch == 'b'){
+                if(!st.empty() && st.top() == 'a'){
+                    st.push('b');
+                }
+                else{
+                    return false;
+                }
+            }
+            else{
+                // ch == 'c'
+                if(!st.empty() && st.top() == 'b'){
+                    st.pop();
+                    if(!st.empty() && st.top() == 'a'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+            }
         }
-        int found  = s.find("abc");
-        if(found != string::npos){
-            // found 
-            string tleft = s.substr(0, found);
-            string tright = s.substr(found+3, s.size());
-            return isValid(tleft + tright);
-        }
-        return false;
+        return st.empty();// ? true : false;
     }
 };
+
+
+
+
+// if(s.size() == 0){
+//             return true;
+//         }
+//         int found  = s.find("abc");
+//         if(found != string::npos){
+//             // found 
+//             string tleft = s.substr(0, found);
+//             string tright = s.substr(found+3, s.size());
+//             return isValid(tleft + tright);
+//         }
+//         return false;
