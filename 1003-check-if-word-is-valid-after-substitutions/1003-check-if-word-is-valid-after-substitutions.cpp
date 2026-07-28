e6772eae7ef87aa90a96cb4782b539738e343393
Aman Khan <amanx11n@gmail.com>
2026-07-28 18:55:17 +0530

    Time: 542 ms (5.04%), Space: 682.9 MB (5.18%) - LeetHub

---
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s.size() == 0){
+            return true;
+        }
+        int found  = s.find("abc");
+        if(found != string::npos){
+            // found 
+            string tleft = s.substr(0, found);
+            string tright = s.substr(found+3, s.size());
+            return isValid(tleft + tright);
+        }
+        return false;
+    }
+};
