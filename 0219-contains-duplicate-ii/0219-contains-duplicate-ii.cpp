5505adab653f570c8c32af7a289c92fe1b090ad7
Aman Khan <amanx11n@gmail.com>
2026-07-29 00:22:31 +0530

    Time: 108 ms (19.39%), Space: 116.9 MB (17.88%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,13 +1,39 @@
 class Solution {
-public:
+public: // using sliding window :
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        for(int i = 0; i<nums.size(); i++){
-            if(mp.find(nums[i]) != mp.end() && abs(mp[nums[i]] - i) <= k)
+        unordered_set<int> st;
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        while(j < n){
+            // step 1- abs(i - j) <= k
+            if(abs(i-j) > k){
+                st.erase(nums[i]);
+                i++; // shrink
+            }
+            //past mai dekha hia nums[j] ?
+            if(st.find(nums[j]) != st.end()){
                 return true;
-            else
-                mp[nums[i]] = i;
+            }
+            st.insert(nums[j]);
+            j++;
         }
         return false;
     }
 };
+
+
+
+// class Solution {
+// public: // using map .
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         unordered_map<int, int> mp;
+//         for(int i = 0; i<nums.size(); i++){
+//             if(mp.find(nums[i]) != mp.end() && abs(mp[nums[i]] - i) <= k)
+//                 return true;
+//             else
+//                 mp[nums[i]] = i;
+//         }
+//         return false;
+//     }
+// };
