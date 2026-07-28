dfef601e9439a37333da41247460ab6d4271ce3c
Aman Khan <amanx11n@gmail.com>
2026-07-29 02:21:05 +0530

    Time: 157 ms (5.02%), Space: 113.5 MB (5.84%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // if(nums.size()== 1){
+        //         return false;
+        //     }
+        // sort(nums.begin(), nums.end());
+        // for(int i=0; i<nums.size()-1; i++){
+        //     //int j= i+1;
+        //     if(nums[i]== nums[i+1]){
+        //         return true;
+        //     }
+        // }
+        // return false;
+        set<int> s;
+        for(int i = 0; i<nums.size(); i++){
+            if(s.find(nums[i]) != s.end()){
+                return true;
+            }
+            else{
+                s.insert(nums[i]);
+            }
+        }
+        return false;
+    }
+};
