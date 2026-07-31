21e0b1d09f028e90ad610b254886b76ece76a7ed
Aman Khan <amanx11n@gmail.com>
2026-07-31 18:53:21 +0530

    Time: 2 ms (85.49%), Space: 14.4 MB (17.56%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> st;
+        string ans = "";
+        for(int i = 0; i<s.length(); i++){
+            char ch = s[i];
+            if(st.empty()){
+                st.push(ch);
+            }
+            else{
+                if(!st.empty() && st.top() == ch){
+                    st.pop();
+                }
+                else{
+                    st.push(ch);
+                }
+            }
+        }
+        while(!st.empty()){
+            int element = st.top();
+            st.pop();
+            ans.push_back(element);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
+
+
+
+
+
+// class Solution {
+// public:
+//     string removeDuplicates(string s) {
+//         string ans= "";
+//         for(int i=0;i<s.length(); i++){
+//             char ch= s[i];
+//             if(ans.empty()){
+//                 ans.push_back(ch);
+//             }
+//             else if(ch != ans.back()){
+//                 ans.push_back(ch);
+//             }
+//             else{
+//                 ans.pop_back();
+//             }
+//         }
+//         return ans;
+//     }
+// };
