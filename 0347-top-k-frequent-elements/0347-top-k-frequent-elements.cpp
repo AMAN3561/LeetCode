fd739e2db485fdb9f1fa85a3368c38f499df618d
Aman Khan <amanx11n@gmail.com>
2026-08-01 16:52:05 +0530

    Time: 0 ms (100%), Space: 17.9 MB (87.32%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,26 +1,61 @@
-class compare{
-    public:
-        bool operator()(pair<int, int> a, pair<int, int> b){
-            return a.second > b.second;
-        }
-};
+// class compare{
+//     public:
+//         bool operator()(pair<int, int> a, pair<int, int> b){
+//             return a.second > b.second;
+//         }
+// };
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+        typedef pair<int, int> P;
         unordered_map<int, int> mp;
-        vector<int> ans;
-        vector<pair<int, int>> vec;
         for(int num : nums){
             mp[num]++;
         }
-        for(auto it : mp){
-            vec.push_back({it.first, it.second});
-        }
-        sort(vec.begin(), vec.end(), compare());
+        // define minheap
+        priority_queue<P, vector<P>, greater<P>> pq; // minheap
 
-        for(int i = 0; i<k; i++){
-            ans.push_back(vec[i].first);
+
+        // push elements iin minheap. 
+        // maintain size of k only 
+        for(auto &it :mp){
+            int value = it.first;
+            int freq = it.second;
+
+            pq.push({freq, value});
+
+            if(pq.size() > k){
+                pq.pop();
+            }
+        }
+        vector<int> ans ;
+        while(!pq.empty()){
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> mp;
+//         vector<int> ans;
+//         vector<pair<int, int>> vec;
+//         for(int num : nums){
+//             mp[num]++;
+//         }
+//         for(auto it : mp){
+//             vec.push_back({it.first, it.second});
+//         }
+//         sort(vec.begin(), vec.end(), compare());
+
+//         for(int i = 0; i<k; i++){
+//             ans.push_back(vec[i].first);
+//         }
+//         return ans;
+//     }
+// };
