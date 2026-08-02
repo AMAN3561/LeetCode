30c5756179b3c7da74aeda540ffac9d2d4fa7946
Aman Khan <amanx11n@gmail.com>
2026-08-02 22:03:35 +0530

    Time: 0 ms (100%), Space: 10.2 MB (98.2%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -1,23 +1,56 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        stack<int> st;
-        st.push(-1);
-        int maxi = 0;
-        for(int i = 0; i<s.length(); i++){
-            if(s[i] == '('){
-                st.push(i);
-            }
+        int left = 0; int right = 0; int maxleng = 0;
+        for(char ch : s){
+            if(ch == '('){
+                left++;
+            } 
             else{
-                st.pop();
-                if(st.empty()){
-                    st.push(i);
-                }
-                else{
-                    maxi = max(maxi, i - st.top());
-                }
+                right++;
+            }
+            if(left == right){
+                maxleng = max(maxleng, 2 * right);
+            }
+            else if(right > left){
+                left = right = 0;
             }
         }
-        return maxi;
+
+        left = right = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            if(s[i] == '('){
+                left++;
+            } 
+            else{
+                right++;
+            }
+            if(left == right){
+                maxleng = max(maxleng, 2 * left);
+            }
+            else if(left > right){
+                left = right = 0;
+            }
+
+        }
+        return maxleng;
+        // stack<int> st; // TC- O(n), SC- O(n)
+        // st.push(-1);
+        // int maxi = 0;
+        // for(int i = 0; i<s.length(); i++){
+        //     if(s[i] == '('){
+        //         st.push(i);
+        //     }
+        //     else{
+        //         st.pop();
+        //         if(st.empty()){
+        //             st.push(i);
+        //         }
+        //         else{
+        //             maxi = max(maxi, i - st.top());
+        //         }
+        //     }
+        // }
+        // return maxi;
     }
 };
