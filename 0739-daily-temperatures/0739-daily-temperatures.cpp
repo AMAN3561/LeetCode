fde5b195dd85f5db061f9c09f0a99fbfdbc28e03
Aman Khan <amanx11n@gmail.com>
2026-08-03 23:37:59 +0530

    Time: 35 ms (11.98%), Space: 111.7 MB (8.46%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -3,7 +3,6 @@ public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         stack<pair<int, int>> st;
         vector<int> ans(temperatures.size(), 0);
-        int gap = 0;
         for (int i = 0; i < temperatures.size(); i++) {
             while (!st.empty() && temperatures[i] > st.top().first) {
                 int index = st.top().second;
