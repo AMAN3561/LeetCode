24a17fc3ca13ce0dc7f22417276cc41a60b1cefd
Aman Khan <amanx11n@gmail.com>
2026-08-04 00:40:16 +0530

    Time: 10 ms (34.29%), Space: 47.5 MB (18.64%) - LeetHub

---
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int getleng(ListNode* head){
+        ListNode* temp = head;
+        int count = 0;
+        while(temp != nullptr){
+            count++;
+            temp = temp->next;
+        }
+        return count;
+    }
+    vector<int> nextLargerNodes(ListNode* head) {
+        vector<int> arr;
+        while(head != nullptr){
+            arr.push_back(head->val);
+            head= head->next;
+        }
+        vector<int> ans(arr.size(), 0);
+        stack<int> st;
+        for(int i = 0; i<arr.size(); i++){
+            while(!st.empty() && arr[i] > arr[st.top()]){
+                int index = st.top();
+                ans[index] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return ans;
+    }
+};
+
+
+    // stack<int> st;
+    // vector<int> ans(temperatures.size(), 0);
+    // for (int i = 0; i < temperatures.size(); i++) {
+    //     while (!st.empty() && temperatures[i] > temperatures[st.top()]) {
+    //         int index = st.top();
+    //         ans[index] = i - index;
+    //         st.pop();
+    //     }
+    //     st.push(i);
+    // }
+    // return ans;
