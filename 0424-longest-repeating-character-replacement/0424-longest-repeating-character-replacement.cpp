9e46603f1698a34261bf1ed1d76fbfb0feb0f886
Aman Khan <amanx11n@gmail.com>
2026-08-11 22:28:27 +0530

    Time: 4 ms (57.07%), Space: 10.9 MB (42.1%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -8,23 +8,52 @@ public:
         for(int right = 0; right<s.length(); right++){
             // update frequency of current character:
             freq[s[right] - 'A']++;
-
             // update maximum frequency:
             maxfreq = max(maxfreq, freq[s[right] - 'A']);
-
             int WindowLength = right - left + 1;
-
             // if replacements needed > k, shrink window:
             if(WindowLength - maxfreq > k){
                 freq[s[left] - 'A']--;
                 left++;
             }
-
             // calculate current window length
             WindowLength = right - left + 1;
-
             maxWindow = max(maxWindow, WindowLength);
         }
         return maxWindow;
     }
 };
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         int freq[26] = {0};
+//         int left = 0;
+//         int right = 0;
+//         int maxfreq = 0;
+//         int maxWindow = 0;
+//         while(right < s.length()) {
+//             // update frequency of current character
+//             freq[s[right] - 'A']++;
+//             // update maximum frequency
+//             maxfreq = max(maxfreq, freq[s[right] - 'A']);
+//             int WindowLength = right - left + 1;
+//             // if replacements needed > k, shrink window
+//             if(WindowLength - maxfreq > k) {
+//                 freq[s[left] - 'A']--;
+//                 left++;
+//             }
+//             // calculate current window length
+//             WindowLength = right - left + 1;
+//             maxWindow = max(maxWindow, WindowLength);
+//             right++;
+//         }
+//         return maxWindow;
+//     }
+// };
