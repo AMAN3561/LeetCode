5750f0ccf6a757f4c881c2d85103e08cc7d360b1
Aman Khan <amanx11n@gmail.com>
2026-08-11 22:05:37 +0530

    Time: 2 ms (71.55%), Space: 11 MB (42.1%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int freq[26] = {0};
+        int left = 0;
+        int maxfreq = 0;
+        int maxWindow = 0;
+        for(int right = 0; right<s.length(); right++){
+            // update frequency of current character:
+            freq[s[right] - 'A']++;
+
+            // update maximum frequency:
+            maxfreq = max(maxfreq, freq[s[right] - 'A']);
+
+            int WindowLength = right - left + 1;
+
+            // if replacements needed > k, shrink window:
+            if(WindowLength - maxfreq > k){
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            // calculate current window length
+            WindowLength = right - left + 1;
+
+            maxWindow = max(maxWindow, WindowLength);
+        }
+        return maxWindow;
+    }
+};
