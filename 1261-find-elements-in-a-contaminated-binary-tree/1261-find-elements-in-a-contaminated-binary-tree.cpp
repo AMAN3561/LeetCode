0381c2a8b1f829c00e65b96a6591fb568a04d143
Aman Khan <amanx11n@gmail.com>
2026-08-13 00:39:37 +0530

    Time: 0 ms (100%), Space: 27.5 MB (75.11%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -1,18 +1,54 @@
+// class FindElements {
+// public: // by DFS:
+//     unordered_set<int> st;
+//     void Dfs(TreeNode* root, int x){
+//         if(root == nullptr){
+//             return;
+//         }
+//         root->val = x;
+//         st.insert(x);
+//         Dfs(root->left, 2*x + 1);
+//         Dfs(root->right, 2*x + 2);
+//     }
+//     FindElements(TreeNode* root) {
+//         st.clear();
+//         Dfs(root, 0);
+//     }
+    
+//     bool find(int target) {
+//         return st.count(target);
+//     }
+// };
+
+
+
+
+
 class FindElements {
-public:
+public: // by BFS:
     unordered_set<int> st;
-    void Dfs(TreeNode* root, int x){
-        if(root == nullptr){
-            return;
-        }
+    void Bfs(TreeNode* root, int x){
+        queue<TreeNode*> q;
         root->val = x;
-        st.insert(x);
-        Dfs(root->left, 2*x + 1);
-        Dfs(root->right, 2*x + 2);
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* temp = q.front();
+            q.pop();
+            st.insert(temp->val);
+
+            if(temp->left != nullptr){
+                temp->left->val = 2* temp->val + 1;
+                q.push(temp->left);
+            }
+            if(temp->right != nullptr){
+                temp->right->val = 2* temp->val + 2;
+                q.push(temp->right);
+            }
+        }
     }
     FindElements(TreeNode* root) {
         st.clear();
-        Dfs(root, 0);
+        Bfs(root, 0);
     }
     
     bool find(int target) {
