cd87be0674bcfaeeaf2a84cdf50ca1c5fd3842eb
Aman Khan <amanx11n@gmail.com>
2026-08-13 23:36:32 +0530

    Time: 3 ms (92.23%), Space: 12.6 MB (56.01%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,26 +1,58 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int freq[26] = {0};
-        int first_pointer = 0;
         int n = answerKey.size();
-        long long  maxConsecutive = 0;
-        long long maxfreq = 0;
-
-        for(int second_pointer = 0; second_pointer<n; second_pointer++){
-            freq[answerKey[second_pointer] - 'A']++;
-
-            maxfreq = max(maxfreq, (long long)freq[answerKey[second_pointer] - 'A']);
-            long long windowlength = second_pointer - first_pointer + 1;
-
-            if(windowlength - maxfreq > k){
-                freq[answerKey[first_pointer] - 'A']--;
-                first_pointer++;
+        int left = 0;
+        int freqT = 0;
+        int freqF = 0;
+        int maxfreq = 0;
+        int ans = 0;
+        for (int right = 0; right < n; right++) {
+            if (answerKey[right] == 'T') {
+                freqT++;
+            } else {
+                freqF++;
             }
-
-            windowlength = second_pointer - first_pointer + 1;
-            maxConsecutive = max(maxConsecutive, windowlength);
+            maxfreq = max(freqT, freqF);
+            int windowlength = right - left + 1;
+            if (windowlength - maxfreq > k) {
+                if (answerKey[left] == 'T') {
+                    freqT--;
+                } else {
+                    freqF--;
+                }
+                left++;
+            }
+            ans = max(ans, right - left + 1);
         }
-        return maxConsecutive;
+        return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int maxConsecutiveAnswers(string answerKey, int k) {
+//         int freq[26] = {0};
+//         int first_pointer = 0;
+//         int n = answerKey.size();
+//         long long  maxConsecutive = 0;
+//         long long maxfreq = 0;
+
+//         for(int second_pointer = 0; second_pointer<n; second_pointer++){
+//             freq[answerKey[second_pointer] - 'A']++;
+
+//             maxfreq = max(maxfreq, (long long)freq[answerKey[second_pointer]
+//             - 'A']); long long windowlength = second_pointer - first_pointer
+//             + 1;
+
+//             if(windowlength - maxfreq > k){
+//                 freq[answerKey[first_pointer] - 'A']--;
+//                 first_pointer++;
+//             }
+
+//             windowlength = second_pointer - first_pointer + 1;
+//             maxConsecutive = max(maxConsecutive, windowlength);
+//         }
+//         return maxConsecutive;
+//     }
+// };
