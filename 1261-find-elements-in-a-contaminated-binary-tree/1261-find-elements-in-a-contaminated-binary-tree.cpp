ffa5c330af483b311d3d7510dbe6393ebc1e2459
Aman Khan <amanx11n@gmail.com>
2026-08-17 21:43:07 +0530

    Time: 13 ms (32.74%), Space: 34.8 MB (20.05%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.cpp
@@ -1,57 +1,53 @@
-// class FindElements {
-// public: // by DFS:
-//     unordered_set<int> st;
-//     void Dfs(TreeNode* root, int x){
-//         if(root == nullptr){
-//             return;
-//         }
-//         root->val = x;
-//         st.insert(x);
-//         Dfs(root->left, 2*x + 1);
-//         Dfs(root->right, 2*x + 2);
-//     }
-//     FindElements(TreeNode* root) {
-//         st.clear();
-//         Dfs(root, 0);
-//     }
-    
-//     bool find(int target) {
-//         return st.count(target);
-//     }
-// };
-
-
-
-
-
 class FindElements {
-public: // by BFS:
+public: // by DFS:
     unordered_set<int> st;
-    void Bfs(TreeNode* root, int x){
-        queue<TreeNode*> q;
-        root->val = x;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* temp = q.front();
-            q.pop();
-            st.insert(temp->val);
-
-            if(temp->left != nullptr){
-                temp->left->val = 2* temp->val + 1;
-                q.push(temp->left);
-            }
-            if(temp->right != nullptr){
-                temp->right->val = 2* temp->val + 2;
-                q.push(temp->right);
-            }
+    void Dfs(TreeNode* root, int x){
+        if(root == nullptr){
+            return;
         }
+        root->val = x;
+        st.insert(x);
+        Dfs(root->left, 2*x + 1);
+        Dfs(root->right, 2*x + 2);
     }
     FindElements(TreeNode* root) {
         st.clear();
-        Bfs(root, 0);
+        Dfs(root, 0);
     }
     
     bool find(int target) {
         return st.count(target);
     }
 };
+
+// class FindElements {
+// public: // by BFS:
+//     unordered_set<int> st;
+//     void Bfs(TreeNode* root, int x){
+//         queue<TreeNode*> q;
+//         root->val = x;
+//         q.push(root);
+//         while(!q.empty()){
+//             TreeNode* temp = q.front();
+//             q.pop();
+//             st.insert(temp->val);
+
+//             if(temp->left != nullptr){
+//                 temp->left->val = 2* temp->val + 1;
+//                 q.push(temp->left);
+//             }
+//             if(temp->right != nullptr){
+//                 temp->right->val = 2* temp->val + 2;
+//                 q.push(temp->right);
+//             }
+//         }
+//     }
+//     FindElements(TreeNode* root) {
+//         st.clear();
+//         Bfs(root, 0);
+//     }
+    
+//     bool find(int target) {
+//         return st.count(target);
+//     }
+// };
