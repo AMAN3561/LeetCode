ff4ec629815743c1529eca2a0eae6fe90c2af183
Aman Khan <amanx11n@gmail.com>
2026-08-19 20:03:54 +0530

    Time: 23 ms (3.67%), Space: 21 MB (5.26%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,28 +1,97 @@
-class Solution {
+// class Solution {
+// public:
+//     string longestCommonPrefix(vector<string>& strs) {
+//         int n = strs.size();
+//         string ans = "";
+//         for(int i = 0; i<strs[0].size(); i++){
+//             char ch = strs[0][i];
+//             bool match = true;
+//             // for comapring ch with rest of the strings :
+//             for(int j = 1; j<n; j++){
+//                 // not match :
+//                 if(strs[j].size() <= i || ch != strs[j][i]){
+//                     match = false;
+//                     break;
+//                 }
+//             }
+//             if(match == false){
+//                 break;
+//             }
+//             else{
+//                 ans.push_back(ch);
+//             }
+//         }
+//         return ans;
+//     }
+// };
+class TrieNode{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int n = strs.size();
+    char data; 
+    unordered_map<char, TrieNode*> children;
+    bool isTerminal;
+    int childcount;
+
+    TrieNode(int val){
+        this->data = val;
+        isTerminal = false;
+        childcount = 0;
+    }
+};
+
+class Solution {
+public: // solving the ques using Tries.
+
+    void insertWord(TrieNode* root, string str){
+        if(str.length() == 0){
+            root->isTerminal = true;
+            return;
+        }
+        char ch = str[0];
+        TrieNode* child;
+        if(root->children.find(ch) != root->children.end()){
+            // present :
+            child = root->children[ch];
+        }
+        else{
+            // absent :
+            child = new TrieNode(ch);
+            root->children[ch] = child;
+
+            root->childcount++;
+        }
+        insertWord(child, str.substr(1));
+    }
+    string findLCP(TrieNode* root, string word){
         string ans = "";
-        for(int i = 0; i<strs[0].size(); i++){
-            char ch = strs[0][i];
+        if(root->isTerminal){
+            return ans;
+        }
+        for(int i = 0; i<word.length(); i++){
+            char ch = word[i];
 
-            bool match = true;
-
-            // for comapring ch with rest of the strings 
-            for(int j = 1; j<n; j++){
-                // not match 
-                if(strs[j].size() <= i || ch != strs[j][i]){
-                    match = false;
-                    break;
-                }
-            }
-            if(match == false){
-                break;
+            // aage mai sirf tabhi move karunga jaab childcount 1 hoga :
+            if(root->childcount == 1){
+                ans.push_back(ch);
+                root = root->children[ch];
             }
             else{
-                ans.push_back(ch);
+                break;
+            }
+            if(root->isTerminal){
+                break;
             }
         }
+        return ans;
+    }
+    string longestCommonPrefix(vector<string>& strs) {
+        TrieNode* root = new TrieNode('-');
+        // insert all the nodes inside the trie
+
+        for(int i = 0; i<strs.size(); i++){
+            string str = strs[i];
+            insertWord(root, str);
+        }
+        string ans = findLCP(root, strs[0]);
         return ans;
     }
 };
