408c170073cdb593b445c26e545d573964f248d8
Aman Khan <amanx11n@gmail.com>
2026-08-20 21:47:15 +0530

    Time: 1 ms (50.11%), Space: 9.4 MB (41.51%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
-    void removeOcc(string &s, string &part){
-        int found = s.find(part);
-        if(found != string::npos){
-            string left_part = s.substr(0, found);
-            string right_part = s.substr(found + part.size(), s.size());
-
-            s = left_part + right_part;
-
-            removeOcc(s, part);
-        }
-        else{
-            // Base case :
-            return;
-        }
-    }
     string removeOccurrences(string s, string part) {
-        removeOcc(s, part);
+        int found = s.find(part);
+        while(found != string::npos){
+            s.erase(found, part.length());
+            found = s.find(part);
+        }
         return s;
     }
 };
+
+
+// class Solution {
+// public:
+//     void removeOcc(string &s, string &part){
+//         int found = s.find(part);
+//         if(found != string::npos){
+                // part string has been found.
+                // remove the part. by making left and right substring wrt part found in the s string.
+//             string left_part = s.substr(0, found);
+//             string right_part = s.substr(found + part.size(), s.size());
+
+//             s = left_part + right_part;
+
+//             removeOcc(s, part);
+//         }
+//         else{
+//             // Base case :
+//             return;
+//         }
+//     }
+//     string removeOccurrences(string s, string part) {
+//         removeOcc(s, part);
+//         return s;
+//     }
+// };
