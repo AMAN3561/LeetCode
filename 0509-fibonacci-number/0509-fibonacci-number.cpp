481c271271ee6b2ef88415fdf11c3e3d837fab1b
Aman Khan <amanx11n@gmail.com>
2026-08-21 18:37:17 +0530

    Time: 3 ms (64.68%), Space: 8.2 MB (17.24%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,16 +1,40 @@
 class Solution {
 public:
-    int fib(int n) {
-        // base cases :-> 
-        if(n == 0){
-            return 0;
-        }
-        if(n == 1){
-            return 1;
-        }
-        // recursive calls :
-        int ans = fib(n-1) + fib(n-2);
+    int solveUsingMemorization(int n, vector<int>& dp){
+        if(n == 0 || n == 1) return n;
 
+        if(dp[n] != -1){
+            // STEP-3 :->
+            // ANS ALREADY EXISTS IN DP ARRAY SO RETURN THE EXISITING ANSWWER :
+            return dp[n];
+        }
+
+        int ans = solveUsingMemorization(n-1, dp) + solveUsingMemorization(n-2, dp);
+        // STEP-2 :-> STORE THE ANSWER IN DP ARRAY SO THAT WE DONT HAVE TO COMPUTE THE SAME CALCULATION AGAIN,
+        // AVOIDING OVERLAPPING SUBPROBLEM.
+        dp[n] = ans;
+        return dp[n];
+    }
+    int fib(int n) {
+        // STEP 1:-> FIGURE OUT THE TYPE OF DP.
+        // CREATE & INITIALISE, PASS THE DP ARRAY
+        vector<int> dp(n+1, -1);
+        int ans = solveUsingMemorization(n, dp);
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int solveUsingRec(int n){
+//         if(n == 0) return 0;
+//         if(n == 1) return 1;
+
+//         int ans = solveUsingRec(n-1) + solveUsingRec(n-2);
+//         return ans;
+//     }
+//     int fib(int n) {
+//         int ans = solveUsingRec(n);
+//         return ans;
+//     }
+// };
