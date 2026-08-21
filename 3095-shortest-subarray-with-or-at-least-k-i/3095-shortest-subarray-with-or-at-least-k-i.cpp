26afa31fe4ab8e9a85e32195b3c585b1966cfb46
Aman Khan <amanx11n@gmail.com>
2026-08-22 01:55:03 +0530

    Time: 1 ms (17.18%), Space: 27.2 MB (86.2%) - LeetHub

--- 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cpp
+++ 3095-shortest-subarray-with-or-at-least-k-i/3095-shortest-subarray-with-or-at-least-k-i.cpp
@@ -3,15 +3,19 @@ public:
     int minimumSubarrayLength(vector<int>& nums, int k) {
         int minimum_Subarray_Length = INT_MAX;
         int n = nums.size();
-        for(int i = 0; i<n; i++){
+        int i = 0;
+        while(i < n){
             int currOR = 0;
-            for(int j = i; j<n; j++){
+            int j = i;
+            while(j < n){
                 currOR |= nums[j];
                 if(currOR >= k){
                     minimum_Subarray_Length = min(minimum_Subarray_Length, j - i + 1);
                     break;
                 }
+                j++;
             }
+            i++;
         }
         return minimum_Subarray_Length == INT_MAX ? -1 : minimum_Subarray_Length;
     }
