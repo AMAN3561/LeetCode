bf3f68bb47dcee8aeb982fb98d5e0e88d7c555cc
Aman Khan <amanx11n@gmail.com>
2026-08-22 14:15:52 +0530

    Time: 79 ms (38.02%), Space: 14.2 MB (24.69%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numSquareHelper(int n, vector<int>& dp){
+        // base case :
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        // 1 case hmm solve karenge :
+        int i = 1;
+        int ans = INT_MAX;
+        int end = sqrt(n);
+        while(i<= end){
+            int perfect_square = i*i;
+            int no_ofperfect_square = 1 + numSquareHelper(n - perfect_square, dp);
+            if(no_ofperfect_square < ans){
+                ans = no_ofperfect_square;
+                dp[n] = ans;
+            }
+            i++;
+        }
+        return dp[n];
+    }
+    int numSquares(int n) {
+        vector<int> dp(n+1, -1);
+        return numSquareHelper(n, dp) - 1;
+    }
+};
