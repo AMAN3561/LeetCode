c792fe9f3512a9da05a3d20ae7311b85e3c52bac
Aman Khan <amanx11n@gmail.com>
2026-09-01 12:42:35 +0530

    Time: 43 ms (57.35%), Space: 46.8 MB (33.83%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -36,11 +36,37 @@ public:
             if (m.find(val) != m.end()) {
                 count += m[val];
             }
-            if (m.find(prefixsum[j]) == m.end()) {
-                m[prefixsum[j]] = 0;
-            }
+            // if (m.find(prefixsum[j]) == m.end()) {
+            //     m[prefixsum[j]] = 0;
+            // }
             m[prefixsum[j]]++;
         }
         return count;
     }
 };
+
+
+// unordered_map<int, int> mp;
+// int count = 0;
+
+// prefixsum[0] = arr[0];
+// for(int i = 1; i<arr.size(); i++){
+//     prefixsum[i] = prefixsum[i-1] + arr[i];
+// }
+
+// for(int j = 0; j<arr.size(); j++){
+//     if(prefixsum[j] == k){
+//         count++;
+//     }
+//     int val = prefixsum[j] - k;
+//     if(mp.find(val) != mp.end()){
+//         count += mp[val];
+//     }
+//     if(mp.find(prefixsum[j]) == mp.end()){
+//         mp[prefixixsum[j]] = 0;
+//     }
+//     mp[prefixsum[j]]++;
+// }
+// return count;
+
+
