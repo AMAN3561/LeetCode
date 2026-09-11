f0e5f77231103773d7d7d44d0b0304d77c42cbc2
Aman Khan <amanx11n@gmail.com>
2026-09-12 01:48:30 +0530

    Time: 3 ms (67.69%), Space: 13.5 MB (68.21%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,39 +1,69 @@
+// better approach :
 class Solution {
 public:
     vector<string> ans;
-    bool isValid(string& curr){
-        int count = 0;
-        for(char ch: curr){
-            if(ch == '('){
-                count++;
-            }
-            else{
-                count--;
-                if(count < 0){
-                    return false;
-                }
-            }
-        }
-        return count == 0;
-    }
-    void solve(string& curr, int n){
+    void solve(string& curr, int n, int open_Bracket, int close_Bracket){
         if(curr.length() == 2*n){
-            if(isValid(curr)){
-                ans.push_back(curr);
-            }
+            ans.push_back(curr);
             return;
         }
-        curr.push_back('(');
-        solve(curr, n);
-        curr.pop_back();
-
-        curr.push_back(')');
-        solve(curr, n);
-        curr.pop_back();
+        if(open_Bracket < n){
+            curr.push_back('(');
+            solve(curr, n, open_Bracket + 1, close_Bracket);
+            curr.pop_back();
+        }
+        if(close_Bracket < open_Bracket){
+            curr.push_back(')');
+            solve(curr, n, open_Bracket, close_Bracket + 1);
+            curr.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         string curr = "";
-        solve(curr, n);
+        int open_Bracket = 0;
+        int close_Bracket = 0;
+        solve(curr, n, open_Bracket, close_Bracket);
         return ans;
     }
 };
+// less better approach :
+
+// class Solution {
+// public:
+//     vector<string> ans;
+//     bool isValid(string& curr){
+//         int count = 0;
+//         for(char ch: curr){
+//             if(ch == '('){
+//                 count++;
+//             }
+//             else{
+//                 count--;
+//                 if(count < 0){
+//                     return false;
+//                 }
+//             }
+//         }
+//         return count == 0;
+//     }
+//     void solve(string& curr, int n){
+//         if(curr.length() == 2*n){
+//             if(isValid(curr)){
+//                 ans.push_back(curr);
+//             }
+//             return;
+//         }
+//         curr.push_back('(');
+//         solve(curr, n);
+//         curr.pop_back();
+
+//         curr.push_back(')');
+//         solve(curr, n);
+//         curr.pop_back();
+//     }
+//     vector<string> generateParenthesis(int n) {
+//         string curr = "";
+//         solve(curr, n);
+//         return ans;
+//     }
+// };
