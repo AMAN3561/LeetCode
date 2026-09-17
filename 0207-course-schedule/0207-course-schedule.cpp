c91578b67fd7f7d50f7e1c6eaf8c730250ba1871
Aman Khan <amanx11n@gmail.com>
2026-09-18 00:41:39 +0530

    Time: 7 ms (46.45%), Space: 22.7 MB (5.22%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,43 +1,90 @@
 class Solution {
 public:
-    bool dfshelper(int src, unordered_map<int, vector<int>>& adjList,
-                   unordered_map<int, bool>& dfstracker,
-                   unordered_map<int, bool>& visited) {
-        visited[src] = true;
-        dfstracker[src] = true;
-
-        for (auto neighbour : adjList[src]) {
-            if (!visited[neighbour]) {
-                bool ans = dfshelper(neighbour, adjList, dfstracker, visited);
-                if (ans == true) {
-                    return true;
-                }
-            } else if (visited[neighbour] == true &&
-                       dfstracker[neighbour] == true) {
-                return true;
+    void topological(int src, unordered_map<int, vector<int>>& adjList, unordered_map<int, int>& Indegree, vector<vector<int>>& prerequisites,queue<int>& q, vector<int>& topoOrder){
+        for(int i = 0; i<src; i++){
+            Indegree[src] = 0;
+        }
+        for(auto it : prerequisites){
+            int u = it[0];
+            int v = it[1];
+            Indegree[v]++;
+        }
+        for(int i = 0; i<src; i++){
+            if(Indegree[i] == 0){
+                q.push(i);
             }
         }
-        dfstracker[src] = false; // backtracking.
-        return false;
+        while(!q.empty()){
+            int front_Node = q.front();
+            q.pop();
+            topoOrder.push_back(front_Node);
+            for(auto neighbour: adjList[front_Node]){
+                Indegree[neighbour]--;
+                if(Indegree[neighbour] == 0){
+                    q.push(neighbour);
+                }
+            }
+        }
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        queue<int> q;
+        vector<int> topoOrder;
         unordered_map<int, vector<int>> adjList;
-        unordered_map<int, bool> dfstracker;
-        unordered_map<int, bool> visited;
+        unordered_map<int, int> Indegree;
         for (auto it : prerequisites) {
             int u = it[0];
             int v = it[1];
 
             adjList[u].push_back(v);
         }
-        for (int src = 0; src < numCourses; src++) {
-            if (!visited[src]) {
-                bool ans = dfshelper(src, adjList, dfstracker, visited);
-                if (ans == true) {
-                    return false;
-                }
-            }
+        topological(numCourses, adjList, Indegree, prerequisites, q, topoOrder);
+        if(topoOrder.size() == numCourses){
+            return true;
+        }else{
+            return false;
         }
-        return true;
     }
 };
+// class Solution {
+// public: //solving by DFS :
+//     bool dfshelper(int src, unordered_map<int, vector<int>>& adjList,
+//                    unordered_map<int, bool>& dfstracker,
+//                    unordered_map<int, bool>& visited) {
+//         visited[src] = true;
+//         dfstracker[src] = true;
+
+//         for (auto neighbour : adjList[src]) {
+//             if (!visited[neighbour]) {
+//                 bool ans = dfshelper(neighbour, adjList, dfstracker, visited);
+//                 if (ans == true) {
+//                     return true;
+//                 }
+//             } else if (visited[neighbour] == true &&
+//                        dfstracker[neighbour] == true) {
+//                 return true;
+//             }
+//         }
+//         dfstracker[src] = false; // backtracking.
+//         return false;
+//     }
+//     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+//         unordered_map<int, vector<int>> adjList;
+//         unordered_map<int, bool> dfstracker;
+//         unordered_map<int, bool> visited;
+//         for (auto it : prerequisites) {
+//             int u = it[0];
+//             int v = it[1];
+
+//             adjList[u].push_back(v);
+//         }
+//         for (int src = 0; src < numCourses; src++) {
+//             if (!visited[src]) {
+//                 bool ans = dfshelper(src, adjList, dfstracker, visited);
+//                 if (ans == true) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+// };
