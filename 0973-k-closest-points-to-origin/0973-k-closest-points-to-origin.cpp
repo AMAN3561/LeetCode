82b7d0e52a7078aa987269967fef42abc781b1b0
Aman Khan <amanx11n@gmail.com>
2026-09-22 00:05:43 +0530

    Time: 83 ms (31.61%), Space: 84.2 MB (11.32%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -1,26 +1,70 @@
-class Solution {
+// class Solution {
+// public:
+//     class comp{
+//         public:
+//             bool operator()(pair<int, int>& a, pair<int,int>& b){
+//                 int distA = a.first*a.first + a.second* a.second;
+//                 int distB = b.first*b.first + b.second* b.second;
+//                 return distA > distB;
+//             }
+//     };
+//     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq; // min heap
+//         vector<vector<int>> ans;
+//         for(auto point : points){
+//             pq.push({point[0], point[1]});
+//         }
+//         while(!pq.empty() && k){
+//             auto topPoints = pq.top();
+//             ans.push_back({topPoints.first, topPoints.second});
+//             pq.pop();
+//             k--;
+//         }
+//         return ans;
+//     }
+// };
+class Comp{
 public:
-    class comp{
-        public:
-            bool operator()(pair<int, int>& a, pair<int,int>& b){
-                int distA = a.first*a.first + a.second* a.second;
-                int distB = b.first*b.first + b.second* b.second;
-                return distA > distB;
-            }
-    };
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, comp> pq; // min heap
+    bool operator()(pair<int, int>& a, pair<int, int>& b){
+        int distA = a.first* a.first + a.second* a.second;
+        int distB = b.first* b.first + b.second* b.second;
+        return distA > distB;
+    }
+};
+class Solution{
+public:
+    vector<vector<int>> kClosest(vector<vector<int>> points, int k){
+        priority_queue<pair<int, int>, vector<pair<int, int>>, Comp> pq;
         vector<vector<int>> ans;
-
-        for(auto point : points){
-            pq.push({point[0], point[1]});
+        for(auto it: points){
+            int first = it[0];
+            int second = it[1];
+            pq.push({first, second});
         }
+
         while(!pq.empty() && k){
-            auto topPoints = pq.top();
-            ans.push_back({topPoints.first, topPoints.second});
+            auto frontelement = pq.top();
+            ans.push_back({frontelement.first, frontelement.second});
             pq.pop();
             k--;
         }
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
