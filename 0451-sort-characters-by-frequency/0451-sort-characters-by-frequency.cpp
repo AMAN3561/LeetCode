1353c2d872434c715200f448b4d9c11f86ffc3f2
Aman Khan <amanx11n@gmail.com>
2026-09-23 01:52:33 +0530

    Time: 3 ms (54.2%), Space: 10.8 MB (77.54%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    typedef pair<int, char> p;
+    string frequencySort(string s) {
+        int n = s.length();
+        string ans = "";
+        unordered_map<char, int> mp;
+        for(auto it: s){
+            mp[it]++;
+        }
+        priority_queue<p, vector<p>> pq;
+        for(auto it: mp){
+            pq.push({it.second, it.first});
+        }
+        while(!pq.empty()){
+            auto front = pq.top();
+            pq.pop();
+            while(front.first){
+                ans.push_back(front.second);
+                front.first--;
+            }
+        }
+        return ans;
+    }
+};
