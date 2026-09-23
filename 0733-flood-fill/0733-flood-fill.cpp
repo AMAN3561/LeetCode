b1df8e47bb6d523c9a6c2b2ae32bcc6a60f3f986
Aman Khan <amanx11n@gmail.com>
2026-09-23 17:31:48 +0530

    Time: 0 ms (100%), Space: 18.7 MB (10.53%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public: 
+    bool isSafe(int new_x, int new_y, vector<vector<int>>& image, vector<vector<bool>>& visited, int old_color){
+        if(new_x >= 0 && new_y >= 0 && new_x < image.size() && new_y < image[0].size() && image[new_x][new_y] == old_color && !visited[new_x][new_y]){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    void dfstraversal(vector<vector<int>>& image, vector<vector<int>>& ans, vector<vector<bool>>& visited, int old_color, int new_color, int sr, int sc){
+        visited[sr][sc] = true;
+        ans[sr][sc] = new_color;
+
+        int dx[] = {-1, 0, 1, 0};
+        int dy[] = {0, 1, 0, -1};
+        for(int i = 0; i<4; i++){
+            int new_x = dx[i] + sr;
+            int new_y = dy[i] + sc;
+            if(isSafe(new_x, new_y, image, visited, old_color)){
+                dfstraversal(image, ans, visited, old_color, new_color, new_x, new_y);
+            }
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        vector<vector<int>> ans = image;
+        vector<vector<bool>> visited(image.size(), vector<bool>(image[0].size(), false));
+        int old_color = image[sr][sc];
+        int new_color = color;
+        // dfs traversal :
+        dfstraversal(image, ans, visited, old_color, new_color, sr, sc);
+        return ans;
+    }
+};
