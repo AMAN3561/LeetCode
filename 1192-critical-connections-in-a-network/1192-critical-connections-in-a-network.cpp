ce60ca10ec90df40c76054ffca10836c25115922
Aman Khan <amanx11n@gmail.com>
2026-09-24 20:41:02 +0530

    Time: 529 ms (5.03%), Space: 232.1 MB (5.14%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    void count_critical_connections(int parent, int src, int& time, unordered_map<int, bool>& visited, vector<int>& insertion_time, vector<int>& low_time, unordered_map<int, vector<int>>& adjList, vector<vector<int>>& ans){
+        time++;
+        visited[src] = true;
+        insertion_time[src] = time;
+        low_time[src] = time;
+
+        for(auto nbr: adjList[src]){
+            if(nbr == parent){
+                continue;
+            }
+            else if(!visited[nbr]){
+                count_critical_connections(src, nbr, time, visited, insertion_time, low_time, adjList, ans);
+                low_time[src] = min(low_time[src], low_time[nbr]);
+                if(low_time[nbr] > insertion_time[src]){
+                    vector<int> temp;
+                    // bridge found: 
+                    temp.push_back(src);
+                    temp.push_back(nbr);
+                    ans.push_back(temp);
+                }
+            }
+            else{
+                low_time[src] = min(low_time[src], low_time[nbr]);
+            }
+        }
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {  
+        vector<vector<int>> ans;
+        int parent = -1;
+        int src = 0; 
+        int time = 0;
+        unordered_map<int, bool> visited;
+        vector<int> insertion_time(n);
+        vector<int> low_time(n);
+
+        unordered_map<int, vector<int>> adjList;
+        for(auto it: connections){
+            int u = it[0];
+            int v = it[1];
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        count_critical_connections(parent, src, time, visited, insertion_time, low_time, adjList, ans);
+        return ans;
+    }
+};
