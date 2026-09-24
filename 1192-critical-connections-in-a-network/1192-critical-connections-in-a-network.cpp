fe13784826ac541b64173861c93e73ddea0486e7
Aman Khan <amanx11n@gmail.com>
2026-09-24 20:59:20 +0530

    Time: 513 ms (5.03%), Space: 232.2 MB (5.14%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -26,7 +26,7 @@ public:
             }
         }
     }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {  
+    vector<vector<int>> criticalConnections(int n, const vector<vector<int>>& connections) {  
         vector<vector<int>> ans;
         int parent = -1;
         int src = 0; 
