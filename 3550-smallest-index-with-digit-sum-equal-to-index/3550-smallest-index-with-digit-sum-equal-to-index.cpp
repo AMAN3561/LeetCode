8687e553ffa4a53f920443e02d417546cb840c7b
Aman Khan <amanx11n@gmail.com>
2026-09-24 23:02:02 +0530

    Time: 17 ms (1.27%), Space: 31.1 MB (15.11%) - LeetHub

--- 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.cpp
@@ -6,12 +6,10 @@ public:
                 cout<<nums[i]<<endl;
                 int sum = 0;
                 int temp = nums[i];
-                vector<int> digits(5);
                 while(temp > 0){
-                    digits.push_back(temp%10);
+                    sum += temp% 10;
                     temp = temp/10;
                 }   
-                sum = accumulate(digits.begin(), digits.end(), 0);
                 if(sum == i){
                     return i;
                 }
