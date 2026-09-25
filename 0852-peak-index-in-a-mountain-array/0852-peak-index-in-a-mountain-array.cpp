bded72dd11063afffd9cfdbdb412c533ae4d2a65
Aman Khan <amanx11n@gmail.com>
2026-09-25 19:33:17 +0530

    Time: 0 ms (100%), Space: 63.4 MB (78.86%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,13 +1,33 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        int ans = 0;
-        auto max_ele = *max_element(arr.begin(), arr.end());
-        for (int i = 0; i < arr.size(); i++) {
-            if(arr[i] == max_ele){
-                ans = i;
+        int strt = 0;
+        int end = arr.size() - 1;
+        while(strt <= end){
+            int mid = strt + (end-strt)/2;
+            if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+            else if(arr[mid] > arr[mid - 1]){
+                strt = mid;
+            }
+            else{
+                end = mid;
             }
         }
-        return ans;
+        return end;
     }
 };
+// class Solution {
+// public:
+//     int peakIndexInMountainArray(vector<int>& arr) {
+//         int ans = 0;
+//         auto max_ele = *max_element(arr.begin(), arr.end());
+//         for (int i = 0; i < arr.size(); i++) {
+//             if(arr[i] == max_ele){
+//                 ans = i;
+//             }
+//         }
+//         return ans;
+//     }
+// };
